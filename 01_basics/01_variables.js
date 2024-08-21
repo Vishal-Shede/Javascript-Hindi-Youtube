4b81b948ac9b10efd6203ec5b6ad0df5b37bd529
Vishal-Shede <vishalshede002@gmail.com>
2024-08-21 11:45:16 +0000

    dataTypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,21 @@
+const accountId = 144553
+let accountEmail = "Vishalshede002@gmail.com"
+var accountPassword = "Vishal@123"
+accountCity = "Karad"
+let accountState;
+
+//accountId = 2   Not Allowed
+
+accountEmail = "vishal@gmail.com"
+accountPassword = "12345"
+accountCity = "Powai"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
+
+
+/*
+Prefernot to use var : bcaz of issue in block scope and funtional scope;
+
+*/
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,41 @@
+"use strict";  //treat all JS code as newer version
+
+//alert(3 + 3)     // we are using nodejs, not browser
+
+console.log(3 
+    + 3)             // Code readiability should be high
+
+console.log("Vishal");
+
+/* Regrading Documentation of JS
+
+recommended mdn    (from mozilla firefox)
+original tc39.es  (from ecma org)
+
+*/
+
+//DataTypes
+
+let name = "Vishal"  //string dataType
+let age = 25         // number
+let isLoggedIn = false      // boolean
+let state;
+
+
+//number   => 2 to pwr 53
+// bigint
+// string  => "" and ''
+// boolean => true/false
+// null    => standalone value  (null is representation of emty value)
+// undefined  =>  
+// symbol  => unique
+
+console.log(typeof "vishal");   //string
+console.log(typeof age);        //number
+console.log(typeof null);       //object
+console.log(typeof undefined);  //undefined
+
+
+
+
+
