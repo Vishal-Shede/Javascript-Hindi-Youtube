82647f4656d7d67d002c05ee74ef64c7f4c7e9ae
Vishal-Shede <vishalshede002@gmail.com>
2024-08-21 12:08:24 +0000

    conversion of datatype

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,35 @@
+let score = "vishal"
+
+console.log(typeof score);
+console.log(typeof (score));  //same things but in function form
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);              
+
+//"33" => 33
+//"33abc"  => NaN
+// true  => 1; false => 0
+// null => 0/NaN
+// undefined => NaN
+
+
+let isLoggedIn = "vishal"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);              
+
+// 1 => true
+// 0 => false
+// "" => false
+// "vishal" => true
+
+//nimber to string converi
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
